9c0c35275c9e6ade71734205b80272e0f3137eeb
Zack Jewell <zack.jewell@gmail.com>
2022-06-11 12:57:06 -0400

    adding theme change

--- _site/_assets/js/main.js
+++ _site/_assets/js/main.js
@@ -1,43 +1,58 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-const prefersLightScheme = window.matchMedia("(prefers-color-scheme: light)");
+const storageKey = 'theme-preference'
 
+const onClick = () => {
+  // flip current value
+  theme.value = theme.value === 'light'
+    ? 'dark'
+    : 'light'
 
-
-
-// if (prefersLightScheme) {
-//     toggleSwitch.checked = false;
-//     localStorage.setItem('theme', 'light');
-//     document.documentElement.setAttribute('data-site-theme', 'light');
-// } else if (prefersDarkScheme) {
-//     toggleSwitch.checked = true;
-//     localStorage.setItem('theme', 'dark');
-//     document.documentElement.setAttribute('data-site-theme', 'dark');
-// }
-
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-site-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        
-        document.documentElement.setAttribute('data-site-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }    
+  setPreference()
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-document.onload = function() {
-    if (prefersLightScheme) {
-    toggleSwitch.checked = false;
-    localStorage.setItem('theme', 'light');
-    document.documentElement.setAttribute('data-site-theme', 'light');
-    } else if (prefersDarkScheme) {
-        toggleSwitch.checked = true;
-        localStorage.setItem('theme', 'dark');
-        document.documentElement.setAttribute('data-site-theme', 'dark');
-    }
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey))
+    return localStorage.getItem(storageKey)
+  else
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light'
 }
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value)
+  reflectPreference()
+}
+
+const reflectPreference = () => {
+  document.firstElementChild
+    .setAttribute('data-theme', theme.value)
+
+  document
+    .querySelector('#theme-toggle')
+    ?.setAttribute('aria-label', theme.value)
+}
+
+const theme = {
+  value: getColorPreference(),
+}
+
+// set early so no page flashes / CSS is made aware
+reflectPreference()
+
+window.onload = () => {
+  // set on load so screen readers can see latest value on the button
+  reflectPreference()
+
+  // now this script can find and listen for clicks on the control
+  document
+    .querySelector('#theme-toggle')
+    .addEventListener('click', onClick)
+}
+
+// sync with system changes
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', ({matches:isDark}) => {
+    theme.value = isDark ? 'dark' : 'light'
+    setPreference()
+  })
