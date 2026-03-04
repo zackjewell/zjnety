1b9b2b950746f4e45c38fd0c161518cc37a08c4d
Zack Jewell <zack.jewell@gmail.com>
2026-03-04 16:11:22 -0500

    rebuilding everything basically

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 export default {
-	title: "The Personal Ramblings of Zack Jewell",
+	title: "The Personal Ramblings of Zachary Jewell",
 	url: "https://www.zackjewell.net/",
 	language: "en",
 	description: "Zack Jewell's special corner of the internet. Learn more about him and his work",
